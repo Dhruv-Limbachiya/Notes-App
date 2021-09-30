df18bb5e3d35b27bb6093806232528d1d55e88cd
Dhruv-Limbachiya <dhruv.mail4work@gmail.com>
2021-09-30 17:42:26 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
